752aa8402b0f1e614b639065010071afb0807257
sonminseong <minseong01222@naver,com>
2024-04-30 19:53:06 +0900

    if

---
+++ if.dart
@@ -0,0 +1,39 @@
+void main() {
+  // int a = 10;
+  // int b = 20;
+
+  // if(a>b) {
+  //   print("a가 b보다 더 크다.");
+  // }
+  // else if (a == b) {
+  //   print("a와 b와 같다");
+  // }
+  // else {
+  //   print("b가 더 크다");
+  // }
+
+  // print(a+b);
+
+  int number = 10;
+  switch(number) {
+    case 5:
+      print("5이다");
+      break;
+    case 6:
+      print("6이다");
+      break;
+    case 7:
+      print("7이다");
+      break;
+    case 8:
+      print("8이다");
+      break;
+    case 9:
+      print("9이다");
+      break;
+   
+    default:
+      print("값이 없다 이말이야");
+
+  }
+}
