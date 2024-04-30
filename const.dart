26d35d277241a7551b2e63aae701eab897a08358
sonminseong <minseong01222@naver,com>
2024-04-30 19:01:50 +0900

    variable

--- const.dart
+++ const.dart
@@ -1,7 +1,6 @@
 void main() {
     final number = 1;
     const number2 = 10;
-    number2 = 11;
     print(number);
     print(number2);
 }
---
+++ string.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "손민성";
+  String hello = "hello";
+  print(name);
+  print(name[name.length - 1]);
+
+  print("${name} ${hello}");
+  print("내이름은 $name 입니다");
+}
