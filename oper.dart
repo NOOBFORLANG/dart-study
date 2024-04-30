333ded8ead7b8344f2eed6753209c8856a5c7bc0
sonminseong <minseong01222@naver,com>
2024-04-30 19:26:20 +0900

    oper

---
+++ oper.dart
@@ -0,0 +1,26 @@
+void main() {
+  int a = 6;
+  int b = 2;
+
+  // print(a >= b);
+
+  // a = a + 3;
+  // print(a);
+  // a++;
+  // print(a);
+  // ++a;
+  // print(a);
+
+  //a*=3; //a = a*3;
+
+  int c = 3;
+  // print(c++); //후위연산 print를 실행 한 후에 c+1
+  // print(c);
+  // print(++c);
+
+  print(c--);
+  print(c);
+  print(--c);
+  
+
+}
